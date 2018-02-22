c5504ed30a87271ec6599fc2af8ef94c83229d17
alvinlai808 <35281838+alvinlai808@users.noreply.github.com>
2018-02-22 10:28:02 -0800

    Completed HighRoll.java

--- homework03/HighRoll.java
+++ homework03/HighRoll.java
@@ -1,17 +1,51 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.*;
 
 public class HighRoll {
 	public static void main( String[] args ) {
 
-		BufferedReader input = new BufferedReader( new InputStreamReader( System.in ) );
+
+		int nDice = 0;
+		int nSides = 0;
+
+    BufferedReader input = new BufferedReader( new InputStreamReader( System.in ) );
+    Scanner sc = new Scanner(System.in);
+
+      try {
+        System.out.println( "Enter the number of dice in set" );
+        nDice = sc.nextInt();
+        while ( nDice < 1 ) {
+          System.out.print( "Error: Please enter an integers at least 1.\n>>" );
+          nDice = sc.nextInt();
+        }
+      } 
+      catch( Exception e ) {
+        System.out.println( "Caught Exception" );
+      }
+
+      try {
+        System.out.println( "Enter the number of sides of each dice" );
+        nSides = sc.nextInt();
+        while ( nSides < 4 ) {
+          System.out.print( "Error: Please enter an integers at least 4.\n>>" );
+          nSides = sc.nextInt();
+        }
+      }
+      catch( Exception e ) {
+        System.out.println( "Caught Exception" );
+      }
+
+
+		DiceSet ds = new DiceSet(nDice, nSides);
+		int maxSoFar = ds.sum();
 
 		System.out.println( "\n   Welcome to the MainProgLoopDemo!!\n" );
-		DiceSet ds = new DiceSet (5, 6);
 
 		while( true ) {
 
+    
 			System.out.println( "Please select your option or press q to quit" );
 			System.out.println( "1. ROLL ALL THE DICE" );
 			System.out.println( "2. ROLL A SINGLE DIE" );
@@ -32,14 +66,40 @@ public class HighRoll {
                		break;
            	 	} else if ( '1' == inputLine.charAt(0) ) {
                		ds.roll();
-           	 	} else if( '2' == inputLine.charAt(0) ) {
-               		ds.rollIndividual();
-           	 	} else if( '3' == inputLine.charAt(0) ) {
-               		ds.sum();
-           	 	} else if( '4' == inputLine.charAt(0) ) {
-               		break;
-           	 	} else if( '5' == inputLine.charAt(0) ) {
-               		break;
+               		System.out.println( "Rolled all dice: " + ds.toString() + "\n" );
+           	 	} else if ( '2' == inputLine.charAt(0) ) {
+           	 		System.out.print("\nWhich die? >> ");
+           	 		
+           	 		try{
+           	 			inputLine = input.readLine();
+           	 			while( inputLine.length() == 0 ){
+           	 				System.out.print( "Error: Please enter at least 1 character.\n>>" );
+           	 				inputLine = input.readLine();
+           	 			}
+
+           	 			int dieToRoll = (int)( inputLine.charAt(0) ) - 48;
+           	 			while ( dieToRoll < 1 || dieToRoll > nDice ) {
+           	 				System.out.print( "Error: Please enter a number between 1 and " + nDice + "\n>>" );
+           	 				inputLine = input.readLine();
+	           	 			dieToRoll = (int)( inputLine.charAt(0) ) - 48;
+           	 			}
+
+           	 			System.out.println( "Before rolling die " + dieToRoll + ": " + ds.toString() );
+           	 			ds.rollIndividual( dieToRoll-1 );
+           	 			System.out.println( "Result: " + ds.getIndividual( dieToRoll - 1 ) );
+           	 			System.out.println( "After rolling die " + dieToRoll + ": " +  ds.toString() );
+
+           	 		} catch( IOException ioe ) {
+			            System.out.println( "Caught IOException" );
+		        	}
+               		System.out.println();
+           	 	} else if ( '3' == inputLine.charAt(0) ) {
+               		System.out.println( "The score for this set is " + ds.sum() + "\n" );
+           	 	} else if ( '4' == inputLine.charAt(0) ) {
+               		maxSoFar = ds.sum();
+               		System.out.println( "Saved " + maxSoFar + " as current high score.\n" );
+           	 	} else if ( '5' == inputLine.charAt(0) ) {
+               		System.out.println( "The saved high score is: " + maxSoFar + "\n" );
            	 	} else {
            	 		System.out.println( "Enter a number between 1 and 5 or press 'q' to quit." );
            	 	}
